92613b2b52406a5caf5dd299cbde7231b59289e8
i.tkachev <itkachev@vk.com>
2020-03-24 23:41:38 +0300

    fix: quick sort depth recursion

--- quick_sort.py
+++ quick_sort.py
@@ -1,9 +1,12 @@
 from typing import Iterable
 from numbers import Number
+import random
+import time
+from copy import deepcopy
 
 
 def quick_sort(array: Iterable[Number], start=None, end=None) -> Iterable[Number]:
-    if start == None and end == None:
+    if start is None and end is None:
         start, end = 0, len(array) - 1
     if start < end:
         p_index = _partition(array, start, end)
@@ -14,7 +17,9 @@ def quick_sort(array: Iterable[Number], start=None, end=None) -> Iterable[Number
 
 
 def _partition(array: Iterable[Number], start: int, end: int) -> int:
-    pivot = array[end]
+    random_pivot_index = random.randint(start, end)
+    pivot = array[random_pivot_index]
+    _swap(array, random_pivot_index, end)
     p_index = start
     for i in range(start, end):
         if array[i] <= pivot:
@@ -31,6 +36,26 @@ def _swap(array, first_index: int, second_index: int):
     array[second_index] = first
 
 
+class Test:
+    def __init__(self, array: Iterable[Number]):
+        self.array = deepcopy(array)
+        t1 = time.time()
+        quick_sort(self.array)
+        print(f'time: {time.time() - t1}')
+        assert sorted(array) == self.array
+
+
 if __name__ == '__main__':
-    test = [1, 4, 2, 3, 32, 5, 9, 1,2, 35, 9, 3, 0]
-    print(quick_sort(test))
+
+    random.seed(13)
+
+    test = [random.randint(0, 100) for _ in range(100)]
+    Test(test)
+    test = [random.randint(0, 4) for _ in range(10)]
+    Test(test)
+    test = [random.randint(0, 1000) for _ in range(10000)]
+    Test(test)
+    test = [random.randint(0, 5) for _ in range(10000)]
+    Test(test)
+    test = [random.randint(0, 1000) for _ in range(10000)]
+    Test(test)
