da2e660fd906e57eb11ddffa06893d787acd9045
i.tkachev <itkachev@vk.com>
2020-04-05 17:29:16 +0300

    feature: count sort

---
+++ count_sort.py
@@ -0,0 +1,37 @@
+from numbers import Number
+from typing import List
+from insertion_sort import insertion_sort
+
+
+def count_sort(array: List[Number]) -> List[Number]:
+    counter = {elem: 0 for elem in insertion_sort(list(set(array)))}
+    for elem in array:
+        counter[elem] += 1
+
+    keys = list(counter.keys())
+    for i in range(1, len(keys)):
+        counter[keys[i]] += counter[keys[i - 1]]
+
+    sorted_array = [0 for _ in range(len(array))]
+    for i in range(len(array) - 1, -1, -1):
+        elem_array = array[i]
+        sorted_array[counter[elem_array] - 1] = elem_array
+        counter[elem_array] -= 1
+
+    return sorted_array
+
+
+if __name__ == '__main__':
+    # test1 = [10, 4, 2, 14, 6, 2, 1, 4, 0]
+    # print(count_sort(test1))
+    # test2 = [2, 3, 9, 2, 9]
+    # print(count_sort(test2))
+
+    def pass_interface():
+        n = input()
+        array = list(map(int, input().split()))
+        sorted_array = count_sort(array)
+        for elem in sorted_array:
+            print(elem, end=' ')
+
+    pass_interface()
--- insertion_sort.py
+++ insertion_sort.py
@@ -2,13 +2,21 @@ from typing import List
 from numbers import Number
 
 
-def insertion_sort(array: List[Number]) -> List[Number]:
+def insertion_sort(array: List[Number], order='asc') -> List[Number]:
     for i in range(len(array)):
         for j in range(len(array)):
-            if array[i] > array[j]:
-                i_elem = array[i]
-                j_elem = array[j]
-                array[j] = i_elem
-                array[i] = j_elem
-
+            if order == 'asc':
+                if array[i] < array[j]:
+                    i_elem = array[i]
+                    j_elem = array[j]
+                    array[j] = i_elem
+                    array[i] = j_elem
+            elif order == 'desc':
+                if array[i] > array[j]:
+                    i_elem = array[i]
+                    j_elem = array[j]
+                    array[j] = i_elem
+                    array[i] = j_elem
     return array
+
+
